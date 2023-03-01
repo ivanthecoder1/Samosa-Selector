3d5f3034c0f97a53e367d6f15635b4170d5c6da7
Ivan Zhang <ivanzhang0111@gmail.com>
2023-02-28 21:47:56 -0500

    added more features

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -1,20 +1,61 @@
 import './App.css';
 import { useState } from 'react';
 
+
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
+
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+      setCount(count - 10);
+    }
+  }
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+      setCount(count - 100);
+    }
+  }
+
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier( multiplier * 10);
+      setCount(count - 1000);
+    }
+  }
 
   return (
     <div className="App">
-      <div className = 'header'>
+      <div className='header'>
         <h1> Samosa Selector</h1>
-        <h2>Count:</h2>
-        <img className = "samosa.jpg" src="https://toppng.com/uploads/preview/transparent-samosas-for-your-blog-frozen-samosa-11562922780nxujrishko.png"></img>
+        <h2>Count: {count}</h2>
+        <img className="samosa.jpg"
+          src="https://png.pngtree.com/png-vector/20210219/ourmid/pngtree-indian-samosa-vector-illustration-cdr-png-image_2923215.jpg"
+          onClick={updateCount}></img>
+        <div className='container'>
+          <div className='upgrade'>
+            <h3>Double Stuffed 👯‍♀️</h3>
+            <p>2x per click</p>
+            <button onClick={buyDoubleStuffed}>10 samosas</button>
+          </div>
+          <div className='upgrade'>
+            <h3>Party Pack 🎉</h3>
+            <p>5x per click</p>
+            <button onClick={buyPartyPack}>100 samosas</button>
+          </div>
+          <div className='upgrade'>
+            <h3>Full Feast 👩🏽‍🍳</h3>
+            <p>10x per click</p>
+            <button onClick={buyFullFeast}>1000 samosas</button>
+          </div>
+          
+        </div>
       </div>
-
-{/* A <div> with the className="header" with the following nested elements:
-An <h1> with the title "Samosa Selector"
-An <h2> with the text "Count:"
-An <img> with the className="samosa" and src set to an image of a samosa (use one from the internet for now). */}
     </div>
   )
 }
